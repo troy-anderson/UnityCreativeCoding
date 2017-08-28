c0543c4d081ea0c2c1530ccf1eb10bbeadf91bac
Troy Anderson <troy.anderson@genesisenergy.co.nz>
2017-08-28 19:10:28 +1200

    TestChanges

--- Assets/sketch.cs
+++ Assets/sketch.cs
@@ -8,26 +8,25 @@ public class sketch : MonoBehaviour {
 
     void Start () {
 
-        int totalCubes = 30;
+        //Create Variable to Store Cube Amounts
+        int totalCubes = 20;
 
         float totaldistance = 2.9f;
         for (int i = 0; i < totalCubes; i++)
         {
             float perc = i / (float)totalCubes;
-
-            float sin = Mathf.Sin(perc * Mathf.PI/2);
-
+            float sin = Mathf.Sin(perc * Mathf.PI / 2);
             float x = 1.8f + sin * totaldistance;
             float y = 5.0f;
             float z = 0.0f;
 
-            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
-            newCube.GetComponent<Renderer>().material.color = Color.blue;
-            newCube.GetComponent<cubeScript>().SetSize(.5f * (1.0f - perc));
-            newCube.GetComponent<cubeScript>().rotateSpeed = .45f + perc*4.0f;
-
-            
-
+            { 
+                var newCube = (GameObject)Instantiate(myPrefab, new Vector3(Random.Range(-4.0f, 4.0f), 2.0f, Random.Range(-4.0f, 4.0f)), Quaternion.identity);
+                newCube.GetComponent<Renderer>().material.color = Color.white;
+                newCube.GetComponent<cubeScript>().SetSize(.5f * (1.0f - perc));
+                //newSphere.GetComponent<cubeScript>().rotateSpeed = .45f + perc * 4.0f;
+                newCube.GetComponent<cubeScript>().AssignCubeNumber(i);
+            }
         }
 	}
 	
---
+++ Assets/sketchSphere.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sketchSphere : MonoBehaviour
+{
+
+    public GameObject myPrefab;
+
+    void Start()
+    {
+
+        //Create Variable to Store Cube Amounts
+        int totalCubes = 20;
+
+        float totaldistance = 2.9f;
+        for (int i = 0; i < totalCubes; i++)
+        {
+            float perc = i / (float)totalCubes;
+            float sin = Mathf.Sin(perc * Mathf.PI / 2);
+            float x = 1.8f + sin * totaldistance;
+            float y = 5.0f;
+            float z = 0.0f;
+
+            {
+                var newSphere = (GameObject)Instantiate(myPrefab, new Vector3(Random.Range(-4.0f, 4.0f), 8.0f, Random.Range(-4.0f, 4.0f)), Quaternion.identity);
+                newSphere.GetComponent<Renderer>().material.color = Color.white;
+                newSphere.GetComponent<cubeScript>().SetSize(.5f * (1.0f - perc));
+                newSphere.GetComponent<cubeScript>().rotateSpeed = .45f + perc * 4.0f;
+            }
+        }
+    }
+
+    void Update()
+    {
+
+
+
+    }
+}
